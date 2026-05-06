1b5d025dc312976124dcae30e0dfc269d9163a1f
JeswinJomi05 <jeswinjomi05@gmail.com>
2026-05-06 21:19:34 +0530

    Test 2

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,18 @@ import cardsRoutes from "./routes/cardRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
-
+if(process.env.NODE_ENV === 'development') {
+    app.use(cors({
+        origin: 'http://localhost:5173',
+    }));
+}
 app.use(cors({
     origin: process.env.VITE_FRONTEND_URL,
 }));
@@ -18,6 +24,15 @@ app.use("/api/notes",notesRoutes);
 app.use("/api/cards",cardsRoutes);
 app.use("/api/tasks",taskRoutes);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+
+
+
+}
 connectDB().then(()=>{
     app.listen(3000, () => {
     console.log('Server is running on port 3000');
