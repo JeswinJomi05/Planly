350407678ad28ea4d30d00264adffa74bf96d299
JeswinJomi05 <jeswinjomi05@gmail.com>
2026-05-06 15:27:26 +0530

    API CHANGED

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import dns from "dns";
 
+import dotenv from 'dotenv';
+dotenv.config();
 dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://jeswinthedeveloper_db_user:UIkTa0rJYXM75QTR@cluster0.o8tpput.mongodb.net/notes_db?appName=Cluster0");
+    await mongoose.connect(process.env.MONGODB_URI);
     
     console.log("MONGODB CONNECTED SUCCESSFULLY!");
   } catch (error) {
