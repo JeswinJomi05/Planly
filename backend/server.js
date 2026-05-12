0de93ee8c310d83db83bcef074595b71e13c7192
JeswinJomi05 <jeswinjomi05@gmail.com>
2026-05-12 17:31:47 +0530

    TEST 5

--- backend/server.js
+++ backend/server.js
@@ -6,34 +6,37 @@ import taskRoutes from "./routes/taskRoutes.js";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-if(process.env.NODE_ENV === 'development') {
-    app.use(cors({
-    origin: process.env.VITE_FRONTEND_URL,
-}));
+if (process.env.NODE_ENV === 'development') {
+  app.use(
+    cors({
+      origin: process.env.VITE_FRONTEND_URL,
+    })
+  );
 }
 
-app.use("/api/notes",notesRoutes);
-app.use("/api/cards",cardsRoutes);
-app.use("/api/tasks",taskRoutes);
+app.use("/api/notes", notesRoutes);
+app.use("/api/cards", cardsRoutes);
+app.use("/api/tasks", taskRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
-
-
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
 }
-connectDB().then(()=>{
-    app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
-})
 
